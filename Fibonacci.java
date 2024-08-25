7e5872081d642dd6cd8f4e40a34ba95ecc5c8b91
Lawrence Ochieng <54819924+shellawk@users.noreply.github.com>
2024-08-25 15:42:59 +0300

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int result = fibonacci(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
+    }
+}
